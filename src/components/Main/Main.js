26508c80a2aead05e4bdb4c87c2c225afa86d0fe
Lawson <lawsonmcphetridge2020@gmail.com>
2022-09-19 17:07:41 -0700

    got filter function to work

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,7 +5,20 @@ import Countries from '../Countries/Countries';
 export default function Main() {
   const { filterCountries, type, setType } = useCountries();
   return (
+      
     <div>
+      <select value={type} onChange={(e) => {
+        setType(e.target.value);
+      }}>
+        <option value="all">All</option>
+        <option value="Oceania">Oceania</option>
+        <option value="Europe">Europe</option>
+        <option value="Africa">Africa</option>
+        <option value="North America">North America</option>
+        <option value="Antarctica">Antarctica</option>
+        <option value="South America">South America</option>
+        <option value="Asia">Asia</option>
+      </select>     
       {filterCountries().map((name) => (
         <Countries key={name.id} {...name} />
       ))} 
